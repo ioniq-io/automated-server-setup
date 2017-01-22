6143687264b2055c5c0be72dac04285ad585a089
phil-ioniq <phil@ics.io>
2017-01-22 12:47:54 -0500

    Work on configuration file import + test new comment format

--- Ubuntu/SetupNodeNgnixLetsEncrypt.sh
+++ Ubuntu/SetupNodeNgnixLetsEncrypt.sh
@@ -1,37 +1,42 @@
-c=l:'c=l: represent an artificial comment line because there is no actual way to put comment in shell scripts.'
+#c=l:'c=l: represent an artificial comment line because there is no actual way to put comment in shell scripts.'
 
-LetsEncrypt_ENABLED="false"
+c=l:'Load configuration'
+config='config.cfg'
+if [ -f ${config} ]; then
+    # check if the file contains something we don't want
+    CFG_SYNTAX="(^\s*#|^\s*$|^\s*[a-z_][^[:space:]]*=[^;&]*$)"
+    if egrep -q -iv "$CONFIG_SYNTAX" "$config"; then
+      echo "[ERROR]: Configuration file format is invalid." >&2
+      exit 1
+    fi
+    # now source it, either the original or the filtered variant
+    source "$config"
+else
+    echo "There is no configuration file call ${config}"
+fi
 
-c=l:'Domain name to configure with initial setup.'
-SITE_DOMAIN="website.com"
 
-c=l:'Entry point file. The name without extension will be used in PM2 to identify the process.'
-SITE_INIT_FILE_NAME="website.js"
-
-c=l:''
-USER_WITH_ROOT_ACCESS="appuser"
-
-c=l:'Change directory to user home directory.'
+#'Change directory to user home directory.'
 cd ~
 
-c=l:'Use curl to download the latest nodejs lts version.'
+#'Use curl to download the latest nodejs lts version.'
 curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
 
-c=l:'Build nodejs source.'
+#'Build nodejs source.'
 sudo bash nodesource_setup.sh
 
-c=l:'Install nodejs from the source we just built, assumes yes for user validation.'
+#'Install nodejs from the source we just built, assumes yes for user validation.'
 sudo apt-get --assume-yes install nodejs
 
-c=l:'Install build essential for npm packages that need to build from source, assumes yes for user validation.'
+#'Install build essential for npm packages that need to build from source, assumes yes for user validation.'
 sudo apt-get --assume-yes install build-essential
 
-c=l:'Go to /var/www/ directory'
+#'Go to /var/www/ directory'
 cd /var/www/
 
-c=l:'Create directory in /var/www/ to contain our app'
+#'Create directory in /var/www/ to contain our app'
 sudo mkdir $SITE_DOMAIN
 
-c=l:'Change directory to the folder we just created in /var/www/'
+#'Change directory to the folder we just created in /var/www/'
 cd $SITE_DOMAIN/
 
