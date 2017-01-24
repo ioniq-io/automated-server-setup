aff019919acba835ec4141c0feff4ffc4564ce55
phil-ioniq <phil@ics.io>
2017-01-23 21:47:39 -0500

    Structure reformat + debugging

--- includes/load/flags.sh
+++ includes/load/flags.sh
@@ -1,1 +1,24 @@
-# Will contain flags overriding the config values on the fly.
+#!/bin/bash
+# Flags
+
+while getopts ":a:" opt; do
+  case $opt in
+    a)
+      echo "-a was triggered, Parameter: $OPTARG" >&2
+      ;;
+    -system)
+      if [ "$OPTARG" = "ubuntu" ]; then
+        echo "System is ubuntu."
+        SYSTEM_TYPE="ubuntu"
+      fi
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
--- setup/setupUbuntu.sh
+++ setup/setupUbuntu.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 source "../includes/load/config.sh"
 SYSTEM_TYPE="ubuntu"
 source "../includes/load/functions.sh"
