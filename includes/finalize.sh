512f8d18bfab101986f79b590480272ea853a43c
phil-ioniq <phil@ics.io>
2017-01-28 22:01:49 -0500

    debug

--- includes/finalize.sh
+++ includes/finalize.sh
@@ -1,3 +1,7 @@
+if [ "$NGINX_ENABLED" = "true" ]; then
+    sudo systemctl stop nginx
+    sudo systemctl start nginx
+fi
 
 echo "Your application is now fully configured."
 echo "You can locate the app file with: cd $WEB_DIRECTORY"
